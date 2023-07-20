f3e645785db3f8d3b7cec04523b5ffc618a8dd49
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-07-20 11:44:26 +0700

    basic types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 fun main() {
-    val name = "Nana"
-    println("Hello $name")
+    val a: Int = 1000
+    val b: String = "Nana"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000
+    val e: Boolean = false
+    val f: Char = 'n'
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
 }
